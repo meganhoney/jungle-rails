8b446978bd9bf1284a60ca63679e293baaf64982
Megan Honey <mjthoney@gmail.com>
2023-01-23 16:54:24 +0000

    Add routes and logic for log in and out

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,24 @@
+class SessionsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    user = User.find_by_email(params[:email])
+    # if the user exists AND the password entered is correct
+    if user && user.authenticate(params[:password])
+      # Save the user id inside the browser cookie
+      # keeps user logged in when they navigate around our website
+      session[:user_id] = user.id
+      redirect_to '/'
+    else
+    # if user login doesn't work, send back to login form
+      redirect_to '/login'
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to '/login'
+  end
+
+end
